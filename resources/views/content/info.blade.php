7995e1b575a2c4b0a8b72b53103e713d2bcf94e8
Vallencius <vallencius1901@gmail.com>
2022-11-11 12:12:49 +0700

    Nama Anggota

--- resources/views/content/info.blade.php
+++ resources/views/content/info.blade.php
@@ -4,10 +4,10 @@
 <div class="text-center" style="padding: 10px">
   <h1>Our member:</h1>
   <p>Vallencius Gavriel Alfredo Siswanto - 00000045651</p>
-  <p>Vallencius Gavriel Alfredo Siswanto - 00000045651</p>
-  <p>Vallencius Gavriel Alfredo Siswanto - 00000045651</p>
-  <p>Vallencius Gavriel Alfredo Siswanto - 00000045651</p>
-  <p>Vallencius Gavriel Alfredo Siswanto - 00000045651</p>
+  <p>Jonathan Putra - 00000054143</p>
+  <p>Christian Alexander - 00000043524</p>
+  <p>Rendy Januardi Winarta - 00000043680</p>
+  <p>Rommy Kusuma Jaya - 00000043919</p>
 </div>
 
 @endsection
