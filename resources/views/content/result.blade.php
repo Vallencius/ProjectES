bd8b8b056645d1d58f57801a04e3d2236f3bcc03
RommyKJ <rommy.kj54@gmail.com>
2022-11-27 17:08:41 +0700

    Design Result
    Buat design result dan treatment.

--- resources/views/content/result.blade.php
+++ resources/views/content/result.blade.php
@@ -1,6 +1,27 @@
 @extends('layout.header')
 
 @section('content')
+<style>
+  .box-res{
+    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+    transition: 0.3s;
+    border-radius: 10px;
+    padding: 10px;
+  }
+
+  hr.new4 {
+    border: 1px solid black;
+  }
+
+  .box-title{
+    text-align: center;
+    margin-top: 10px;
+    margin-bottom: 10px;
+  }
+
+
+</style>
+
 <div class="container">
   <h1 class="text-center">Diagnosys Result</h1>
   <table class="table table-bordered table-striped">
@@ -23,8 +44,12 @@
       <td>{{ $CF[3] }} %</td>
     </tr>
   </table>
-  <h3>Certainty Value: {{ $value }} %</h3>
-  <h3>Disease: {{ $penyakit }}</h3>
-  <h3>Treatment:<br/> {!! $treatment !!}</h3>
+  <div class="box-res"> 
+      <h2 style="text-align: center"><strong>{{ $penyakit }} </strong></h1>
+      <h3 style="text-align: center">Certainty Value: <strong>{{ $value }} % </strong></h2>
+      <hr class="new4" />
+      <h3 class="box-title"><strong>Treatment</strong></h3>
+        <h4 style="text-align: center">{!! $treatment !!} </h4>
+  </div>
 </div>
 @endsection
